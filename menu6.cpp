823df959cf523f66afa7a1e329637c2a7b5ee41e
ecourse <smkang@codenuri.co.kr>
2023-11-10 15:41:12 +0900

    Add files via upload

--- menu6.cpp
+++ menu6.cpp
@@ -3,6 +3,21 @@
 #include <string>
 #include <conio.h>
 
+// 객체지향 언어에서 프로그램이란 !!
+
+// 1. 객체를 만들고
+// 2. 객체간의 관계를 설정하고
+// 3. 객체간의 메세지를 주고 받는 개념이다.(멤버 함수를 호출한다는 의미)
+// 4. 프로그램의 기본 단위는 "함수" 가 아닌 "클래스" 이다
+
+// 장점 : 확장성이 뛰어나고, 변화에 대응하기 좋다.!!
+
+// 단점 : 어렵다!!
+//       메모리 사용량이 많아 질수 있다.
+//       (사용하지 않은 멤버가 메모리에 놓일수 있고 가상함수의 오버헤드도 있다)
+
+
+
 class BaseMenu
 {
 	std::string title;
@@ -74,10 +89,13 @@ int main()
 	PopupMenu* pm2 = new PopupMenu("해상도 변경");
 
 	root->add(pm1);
-	root->add(pm2);
+//	root->add(pm2);
+	pm1->add(pm2);
+
 
 	pm1->add( new MenuItem("RED",  11));
 	pm1->add( new MenuItem("BLUE", 12));
+	pm1->add( new MenuItem("GREEN", 13));
 
 	pm2->add( new MenuItem("HD",  21));
 	pm2->add( new MenuItem("FHD", 22));
