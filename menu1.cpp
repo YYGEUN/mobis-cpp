94964596478568f8eea6fc24a1f142e4879b97f4
ecourse <smkang@codenuri.co.kr>
2023-11-10 14:18:19 +0900

    Add files via upload

---
+++ menu1.cpp
@@ -0,0 +1,21 @@
+// menu1.cpp
+#include <iostream>
+#include <vector>
+#include <string>
+#include <conio.h>
+
+int main()
+{
+	printf("1. 기기등록\n");
+	printf("2. 기기삭제\n");
+	printf("메뉴를 선택하세요 >> ");
+
+	int cmd;
+	scanf_s("%d", &cmd);
+
+	switch( cmd )
+	{
+		case 1: break;
+		case 2: break;
+	}
+}
