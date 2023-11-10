260129e8dc9126e1915c57ae2e9224e6e60c6467
ecourse <smkang@codenuri.co.kr>
2023-11-10 13:35:54 +0900

    Add files via upload

--- shape3.cpp
+++ shape3.cpp
@@ -42,15 +42,22 @@ int main()
 
 		if ( cmd == 1 )
 		{
-			Rect rc(0, 0,100,100);
+//			Rect rc(0, 0,100,100); // 이렇게 만들면 {} 벗어나면 즉시 파괴 됩니다
+
+			Rect* p = new Rect(0, 0, 100, 100); // 이렇게 만들고
+			v.push_back(p); // 주소를 v에 보관
 		}
 		else if ( cmd == 2 )
 		{
-
+			// 위 코드는 그냥 아래 처럼 하면 됩니다.
+			v.push_back( new Circle( 0, 0, 50));
 		}
 		else if ( cmd == 9 )
 		{
-			
+			for( auto p : v)
+				p->draw(); 		// 왜 에러인가요 ??
+								// 잘 생각해 보세요.
+								// 해결책은 다음 소스에서!!
 		}
 	}
 
